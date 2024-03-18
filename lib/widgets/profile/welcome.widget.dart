d02b9a600950f584434622b73283f4cdcd037e1b
cuong2301 <gygybu88@gmail.com>
2024-03-18 19:04:45 +0700

    change ui Welcome page

--- lib/widgets/profile/welcome.widget.dart
+++ lib/widgets/profile/welcome.widget.dart
@@ -18,67 +18,64 @@ class _WelcomeWidget extends ConsumerState<WelcomeWidget> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 70),
-                const Icon(
-                  Icons.linked_camera,
-                  color: Color.fromARGB(255, 0, 0, 0),
-                  size: 50,
-                  semanticLabel: 'Text to announce in accessibility modes',
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 70),
+              Image.asset(
+                'assets/img/123.jpg',
+                width: 360,
+                height: 200,
+              ),
+              const SizedBox(height: 30),
+              const Text(
+                'Welcome Huy!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  //fontWeight: FontWeight.bold,
                 ),
-                const SizedBox(height: 30),
-                const Text(
-                  'Welcome Hai!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    //fontWeight: FontWeight.bold,
-                  ),
+              ),
+              const Text(
+                "Let's start with your frist project post  ",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  //fontWeight: FontWeight.bold,
                 ),
-                const Text(
-                  "Let's start with your frist project post  ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 16,
-                    //fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 30),
-                Container(
-                  alignment: Alignment.center,
-                  child: SizedBox(
-                    height: 50,
-                    width: 145,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        ref
-                            .read(optionsProvider.notifier)
-                            .setWidgetOption('Dashboard');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                          side: const BorderSide(color: Colors.black),
-                        ),
-                        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+              ),
+              const SizedBox(height: 30),
+              Container(
+                alignment: Alignment.center,
+                child: SizedBox(
+                  height: 50,
+                  width: 145,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      ref
+                          .read(optionsProvider.notifier)
+                          .setWidgetOption('Dashboard');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                        side: const BorderSide(color: Colors.black),
                       ),
-                      child: const Text(
-                        'Get started!',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Color.fromARGB(255, 255, 255, 255),
-                          fontWeight: FontWeight.w500,
-                        ),
+                      backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+                    ),
+                    child: const Text(
+                      'Get started!',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
